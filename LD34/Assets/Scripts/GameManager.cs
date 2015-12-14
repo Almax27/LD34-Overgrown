0926a22bb28d6a348208295f448f22552eedc0d7
Aaron <almaxmasuma@gmail.com>
2015-12-14 01:27:13 +0000

    Created GameManager and added music
    http://cloudkickermusic.com/track/hassan-2
    Thanks cloudkicker!

---
+++ LD34/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public AudioSource musicPrefab;
+    AudioSource music;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (musicPrefab)
+        {
+            music = Instantiate(musicPrefab.gameObject).GetComponent<AudioSource>();
+            music.transform.parent = transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
